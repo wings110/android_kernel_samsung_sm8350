552301f8b1beeb5b79f98a1e475509b30fdb4200
LuK1337 <priv.luk@gmail.com>
2024-09-06 09:42:57 +0000

    drivers: sec_input: Use KEY_WAKEUP for DT2W
    Change-Id: I3aedc906506f75d4576b41c668629151fbc75285

--- drivers/input/sec_input/sec_common_fn.c
+++ drivers/input/sec_input/sec_common_fn.c
@@ -590,9 +590,9 @@ void sec_input_gesture_report(struct device *dev, int id, int x, int y)
 	pdata->gesture_x = x;
 	pdata->gesture_y = y;
 
-	input_report_key(pdata->input_dev, KEY_BLACK_UI_GESTURE, 1);
+	input_report_key(pdata->input_dev, KEY_WAKEUP, 1);
 	input_sync(pdata->input_dev);
-	input_report_key(pdata->input_dev, KEY_BLACK_UI_GESTURE, 0);
+	input_report_key(pdata->input_dev, KEY_WAKEUP, 0);
 	input_sync(pdata->input_dev);
 
 	if (id == SPONGE_EVENT_TYPE_SPAY) {
@@ -910,7 +910,7 @@ static void sec_input_set_prop(struct device *dev, struct input_dev *input_dev, 
 	set_bit(BTN_TOOL_FINGER, input_dev->keybit);
 	set_bit(BTN_PALM, input_dev->keybit);
 	set_bit(BTN_LARGE_PALM, input_dev->keybit);
-	set_bit(KEY_BLACK_UI_GESTURE, input_dev->keybit);
+	set_bit(KEY_WAKEUP, input_dev->keybit);
 	set_bit(KEY_INT_CANCEL, input_dev->keybit);
 
 	set_bit(propbit, input_dev->propbit);
@@ -951,7 +951,7 @@ static void sec_input_set_prop2(struct device *dev, struct input_dev *input_dev,
 	set_bit(BTN_TOOL_FINGER, input_dev->keybit);
 	set_bit(BTN_PALM, input_dev->keybit);
 	set_bit(BTN_LARGE_PALM, input_dev->keybit);
-	set_bit(KEY_BLACK_UI_GESTURE, input_dev->keybit);
+	set_bit(KEY_WAKEUP, input_dev->keybit);
 	set_bit(KEY_INT_CANCEL, input_dev->keybit);
 
 	set_bit(propbit, input_dev->propbit);
@@ -992,7 +992,7 @@ static void sec_input_set_prop_pad(struct device *dev, struct input_dev *input_d
 	set_bit(BTN_TOOL_FINGER, input_dev->keybit);
 	set_bit(BTN_PALM, input_dev->keybit);
 	set_bit(BTN_LARGE_PALM, input_dev->keybit);
-	set_bit(KEY_BLACK_UI_GESTURE, input_dev->keybit);
+	set_bit(KEY_WAKEUP, input_dev->keybit);
 	set_bit(KEY_INT_CANCEL, input_dev->keybit);
 
 	set_bit(propbit, input_dev->propbit);
